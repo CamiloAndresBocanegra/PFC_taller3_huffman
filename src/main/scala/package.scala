6b74201d0c82922eed732d9f5eabcbfdaf3751a5
JuanPabloAnteSuarez03 <115759855+JuanPabloAnteSuarez03@users.noreply.github.com>
2023-04-24 18:12:34 -0500

    Update package.scala

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -2,7 +2,7 @@ package object Huffman {
 	abstract class ArbolH
 	case class Nodo(izq: ArbolH, der: ArbolH, cars: List[Char], peso: Int) extends ArbolH
 	case class Hoja(car: Char, peso: Int) extends ArbolH
-
+//Parte 1 : Funciones esenciales y sencillas
 	def peso(arbol: ArbolH): Int = arbol match{
 		case Nodo(i, d, c, peso) => peso
 		case Hoja(car, peso) => peso
@@ -15,7 +15,12 @@ package object Huffman {
 
 	def hacerNodoArbolH(izq: ArbolH, der: ArbolH) =
 		Nodo(izq, der, cars(izq) ::: cars(der), peso(izq) + peso(der))
-
+//Parte 2:Construyendo Arboles de Huffman
+/**
+* En este taller estamos trabajando con listas de caracteres.
+* La funcion cadenaALista crea una lista de caracteres correspondiente a una cadena dada
+*/
+	def cadenaALista (cad : String) : List[Char] = cad.toList
 	def ocurrencias(cars: List[Char]): List[(Char, Int)] = {
 		def contar_car_en_lista(c:Char, l:List[Char]):Int = {
 			def contar_car_en_lista_aux(c:Char, l:List[Char], result:Int):Int = {
@@ -145,19 +150,83 @@ package object Huffman {
 		}
 	}
 
-	def hastaQue(cond:List[ArbolH]=>Boolean, mezclar:List[ArbolH]=>List[ArbolH] )(listaOrdenadaArboles:List[ArbolH]): List[ArbolH] = {
-		def aux(cond:List[ArbolH]=>Boolean, mezclar:List[ArbolH]=>List[ArbolH], result:List[ArbolH]):List[ArbolH] = {
-			if(cond(result))
-				result
-			else
-				aux(cond, mezclar, mezclar(result))
+	def hastaQue(cond: List[ArbolH] => Boolean, mezclar: List[ArbolH] => List[ArbolH])(listaOrdenadaArboles: List[ArbolH]): List[ArbolH] = {
+		def iterar(lista: List[ArbolH]): List[ArbolH] = {
+			if (cond(lista)) {
+			lista
+			} else {
+			iterar(mezclar(lista))
+			}
 		}
-		aux(cond, mezclar, listaOrdenadaArboles)
+
+		iterar(listaOrdenadaArboles)
+		}
+	
+	def crearArbolDeHuffman(cars: List[Char]): ArbolH = {
+		val frecuencias = ocurrencias(cars)
+		val hojas = listaDeHojasOrdenadas(frecuencias)
+		hastaQue(listaUnitaria, combinar)(hojas).head
+	}
+//Parte 3:Decodificar
+	type Bit = Int
+	def decodificar(arbol: ArbolH, bits: List[Bit]): List[Char] = {
+		def decodificarRec(subarbol: ArbolH, bitsRestantes: List[Bit], resultado: List[Char]): List[Char] = subarbol match {
+			case Hoja(caracter, _) => decodificarRec(arbol, bitsRestantes, resultado :+ caracter)
+			case Nodo(izquierda, derecha, _, _) => bitsRestantes.headOption match {
+			case Some(0) => decodificarRec(izquierda, bitsRestantes.tail, resultado)
+			case Some(1) => decodificarRec(derecha, bitsRestantes.tail, resultado)
+			case _ => resultado 
+			}
+		}
+		decodificarRec(arbol, bits, List.empty[Char])
+	}
+//Parte 4a:Codificando usando arboles de Huffman
+	def codificar(arbol: ArbolH)(texto: List[Char]): List[Bit] = {
+		def buscar(car: Char, arbol: ArbolH): List[Bit] = arbol match {
+			case Hoja(_, _) => Nil
+			case Nodo(izq, der, _, _) =>
+			if (contieneCaracter(car, izq)) 0 :: buscar(car, izq)
+			else 1 :: buscar(car, der)
+		}		
+		def contieneCaracter(car: Char, arbol: ArbolH): Boolean = arbol match {
+			case Hoja(c, _) => car == c
+			case Nodo(izq, der, _, _) => contieneCaracter(car, izq) || contieneCaracter(car, der)
+		}
+		texto.flatMap(caracter => buscar(caracter, arbol))
 	}
 
-	def crearArbolDeHuffman(cars:List[Char]):ArbolH = {
-		val frecuencias = ocurrencias(cars)
-		val lista_de_hojas = listaDeHojasOrdenadas(frecuencias)
-		hastaQue(listaUnitaria, combinar)(lista_de_hojas).head
+	type TablaCodigos = List[(Char, List[Bit])]
+	def codigoEnBits(tabla: TablaCodigos)(car: Char): List[Bit] = {
+	tabla.find(_._1 == car) match {
+		case Some((_, bits)) => bits
+		case None => Nil
 	}
+	}
+	
+	def mezclarTablasDeCodigos(a: TablaCodigos, b: TablaCodigos): TablaCodigos = {
+		def agregarPrefijo(cod: List[Bit], pref: Bit): List[Bit] = pref :: cod
+		
+		val tablaA = a.map { case (c, cod) => (c, agregarPrefijo(cod, 0)) }
+		val tablaB = b.map { case (c, cod) => (c, agregarPrefijo(cod, 1)) }
+		
+		tablaA ++ tablaB
+	}
+
+	def convertir(arbol: ArbolH): TablaCodigos = arbol match {
+		case Hoja(car, _) => List((car, List()))
+		case Nodo(izq, der, _, _) => {
+			val tablaIzq = convertir(izq)
+			val tablaDer = convertir(der)
+			mezclarTablasDeCodigos(tablaIzq, tablaDer).map{case (car, bits) =>
+			if (tablaIzq.exists(_._1 == car)) (car, 0 :: bits)
+			else (car, 1 :: bits)
+			}
+		}
+	}
+
+	def codificarRapido(arbol: ArbolH)(texto: List[Char]): List[Bit] = {
+		val tabla = convertir(arbol)
+		texto.flatMap(codigoEnBits(tabla))
+	}
+
 }
